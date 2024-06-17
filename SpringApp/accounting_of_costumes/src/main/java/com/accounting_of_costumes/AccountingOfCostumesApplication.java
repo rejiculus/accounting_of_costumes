83a90090664bacb44f0efe4ae9a1de78b30b04d2
bez <nik_bezdomnii@gmail.com>
2024-06-17 19:14:03 +0300

    spring init

---
+++ SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/AccountingOfCostumesApplication.java
@@ -0,0 +1,13 @@
+package com.accounting_of_costumes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingOfCostumesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingOfCostumesApplication.class, args);
+	}
+
+}
---
+++ SpringApp/accounting_of_costumes/src/test/java/com/accounting_of_costumes/AccountingOfCostumesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting_of_costumes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingOfCostumesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
