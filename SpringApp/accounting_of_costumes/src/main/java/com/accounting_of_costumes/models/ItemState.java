9780d3218b80b853be5ad7a57f8b57e32be01f58
bez <nik_bezdomnii@gmail.com>
2024-06-19 00:28:08 +0300

    Realize itemstate model and service

---
+++ SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/models/ItemState.java
@@ -0,0 +1,29 @@
+package com.accounting_of_costumes.models;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name="item_state")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class ItemState {
+
+    @Id
+    @Column(name="state_name",nullable=false)
+    private String name;
+
+    @OneToMany(mappedBy = "state")
+    private Set<Item> items;
+}
