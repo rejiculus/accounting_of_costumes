5aaab718412a0816f4148f1794b4c76f2488eb62
Bezdomnii <nik.bezdomnii@gmail.com>
2024-07-15 22:14:24 +0300

    Modified PlaceRepository
    add findByName method

--- SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/db/repository/PlaceRepository.java
+++ SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/db/repository/PlaceRepository.java
@@ -3,5 +3,8 @@ package com.accounting_of_costumes.api.config.db.repository;
 import com.accounting_of_costumes.api.config.db.schema.PlaceSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlaceRepository extends JpaRepository<PlaceSchema, Long> {
+    Optional<PlaceSchema> findByName(String name);
 }
