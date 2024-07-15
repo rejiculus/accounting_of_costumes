bc4929467c2a34e1f92bde6e85fc141bb54cc0a0
Bezdomnii <nik.bezdomnii@gmail.com>
2024-07-15 22:13:39 +0300

    modified MvcConfig
    add created controllers

--- SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/web/MvcConfig.java
+++ SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/web/MvcConfig.java
@@ -1,25 +1,49 @@
 package com.accounting_of_costumes.api.config.web;
 
+import com.accounting_of_costumes.api.Place.Gateway.PlaceDatabaseGateway;
 import com.accounting_of_costumes.api.Tag.Gateway.TagDatabaseGateway;
+import com.accounting_of_costumes.api.config.db.repository.PlaceRepository;
 import com.accounting_of_costumes.api.config.db.repository.TagRepository;
+import com.accounting_of_costumes.entities.Place.gateway.PlaceGateway;
 import com.accounting_of_costumes.entities.Tag.gateway.TagGateway;
+import com.accounting_of_costumes.usercases.Place.GetAllPlacesUserCase;
 import com.accounting_of_costumes.usercases.Tag.CreateTagUserCase;
+import com.accounting_of_costumes.usercases.Tag.DeleteTagUserCase;
 import com.accounting_of_costumes.usercases.Tag.GetAllTagsUserCase;
+import com.accounting_of_costumes.usercases.Tag.GetTagUserCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MvcConfig {
 
+    /*      Tags        */
     @Bean
     public GetAllTagsUserCase getAllTagsUserCase(TagRepository tagRepository){
-        TagGateway TagGateway = new TagDatabaseGateway(tagRepository);
-        return new GetAllTagsUserCase(TagGateway);
+        TagGateway tagGateway = new TagDatabaseGateway(tagRepository);
+        return new GetAllTagsUserCase(tagGateway);
     }
 
     @Bean
     public CreateTagUserCase createTagUserCase(TagRepository tagRepository){
-        TagGateway TagGateway = new TagDatabaseGateway(tagRepository);
-        return new CreateTagUserCase(TagGateway);
+        TagGateway tagGateway = new TagDatabaseGateway(tagRepository);
+        return new CreateTagUserCase(tagGateway);
+    }
+    @Bean
+    public GetTagUserCase getTagUserCase(TagRepository tagRepository){
+        TagGateway tagGateway = new TagDatabaseGateway(tagRepository);
+        return new GetTagUserCase(tagGateway);
+    }
+    @Bean
+    public DeleteTagUserCase deleteTagUserCase(TagRepository tagRepository){
+        TagGateway tagGateway = new TagDatabaseGateway(tagRepository);
+        return new DeleteTagUserCase(tagGateway);
+    }
+
+    /*      Places      */
+    @Bean
+    public GetAllPlacesUserCase getAllPlacesUserCase(PlaceRepository placeRepository){
+        PlaceGateway placeGateway = new PlaceDatabaseGateway(placeRepository);
+        return new GetAllPlacesUserCase(placeGateway);
     }
 }
