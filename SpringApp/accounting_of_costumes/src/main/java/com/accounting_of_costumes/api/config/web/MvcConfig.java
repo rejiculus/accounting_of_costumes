5f35a52f1b4490aa472a7623f20fdde7e89d40a4
Bezdomnii <nik.bezdomnii@gmail.com>
2024-07-16 22:27:03 +0300

    Add Place usercase beans in config

--- SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/web/MvcConfig.java
+++ SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/web/MvcConfig.java
@@ -1,12 +1,13 @@
 package com.accounting_of_costumes.api.config.web;
 
+import com.accounting_of_costumes.api.Place.Controller.CreatePlaceController;
 import com.accounting_of_costumes.api.Place.Gateway.PlaceDatabaseGateway;
 import com.accounting_of_costumes.api.Tag.Gateway.TagDatabaseGateway;
 import com.accounting_of_costumes.api.config.db.repository.PlaceRepository;
 import com.accounting_of_costumes.api.config.db.repository.TagRepository;
 import com.accounting_of_costumes.entities.Place.gateway.PlaceGateway;
 import com.accounting_of_costumes.entities.Tag.gateway.TagGateway;
-import com.accounting_of_costumes.usercases.Place.GetAllPlacesUserCase;
+import com.accounting_of_costumes.usercases.Place.*;
 import com.accounting_of_costumes.usercases.Tag.CreateTagUserCase;
 import com.accounting_of_costumes.usercases.Tag.DeleteTagUserCase;
 import com.accounting_of_costumes.usercases.Tag.GetAllTagsUserCase;
@@ -41,9 +42,41 @@ public class MvcConfig {
     }
 
     /*      Places      */
+
+
+    @Bean
+    public CreatePlaceUserCase createPlaceUserCase(PlaceRepository placeRepository){
+        PlaceGateway placeGateway = new PlaceDatabaseGateway(placeRepository);
+        return new CreatePlaceUserCase(placeGateway);
+    }
+
+    @Bean
+    public DeletePlaceUserCase deletePlaceUserCase(PlaceRepository placeRepository){
+        PlaceGateway placeGateway = new PlaceDatabaseGateway(placeRepository);
+        return new DeletePlaceUserCase(placeGateway);
+    }
+
     @Bean
     public GetAllPlacesUserCase getAllPlacesUserCase(PlaceRepository placeRepository){
         PlaceGateway placeGateway = new PlaceDatabaseGateway(placeRepository);
         return new GetAllPlacesUserCase(placeGateway);
+    }
+
+    @Bean
+    public GetPlaceByIdUserCase getPlaceByIdUserCase(PlaceRepository placeRepository){
+        PlaceGateway placeGateway = new PlaceDatabaseGateway(placeRepository);
+        return new GetPlaceByIdUserCase(placeGateway);
+    }
+
+    @Bean
+    public GetPlaceByNameUserCase getPlaceByNameUserCase(PlaceRepository placeRepository){
+        PlaceGateway placeGateway = new PlaceDatabaseGateway(placeRepository);
+        return new GetPlaceByNameUserCase(placeGateway);
+    }
+
+    @Bean
+    public UpdatePlaceUserCase updatePlaceUserCase(PlaceRepository placeRepository){
+        PlaceGateway placeGateway = new PlaceDatabaseGateway(placeRepository);
+        return new UpdatePlaceUserCase(placeGateway);
     }
 }
