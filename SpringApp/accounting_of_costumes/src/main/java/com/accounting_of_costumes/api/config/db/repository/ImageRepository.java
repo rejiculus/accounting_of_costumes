b0341c67cf61fedbac33954ec03439349431d26e
Bezdomnii <nik.bezdomnii@gmail.com>
2024-07-24 13:52:04 +0300

    Add methods into repositories interfaces

--- SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/db/repository/ImageRepository.java
+++ SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/db/repository/ImageRepository.java
@@ -1,7 +1,14 @@
 package com.accounting_of_costumes.api.config.db.repository;
 
 import com.accounting_of_costumes.api.config.db.schema.ImageSchema;
+import com.accounting_of_costumes.api.config.db.schema.ItemSchema;
+import com.accounting_of_costumes.entities.Image.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ImageRepository extends JpaRepository<ImageSchema, Long> {
+    Optional<ImageSchema> findBySource(String source);
+    List<ImageSchema> findByItem(ItemSchema item);
 }
--- SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/db/repository/LocationRepository.java
+++ SpringApp/accounting_of_costumes/src/main/java/com/accounting_of_costumes/api/config/db/repository/LocationRepository.java
@@ -1,7 +1,13 @@
 package com.accounting_of_costumes.api.config.db.repository;
 
 import com.accounting_of_costumes.api.config.db.schema.LocationSchema;
+import com.accounting_of_costumes.api.config.db.schema.PlaceSchema;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<LocationSchema, Long> {
+    Optional<LocationSchema> findByRoute(String route);
+    List<LocationSchema> findByPlace(PlaceSchema place);
 }
